39b5ac0fffd659fd709185729ea997b4ffc356ae
sensre <32806443+sensre@users.noreply.github.com>
2022-08-24 03:34:30 -0400

    Delete k8s-spec-scripts directory

--- k8s-spec-scripts/debug-pods/pod-stop/delete-pod-loop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-
-while [ 1 ]; do sleep 10 ; kubectl get pods | grep nginx | awk '{print $1}' | xargs kubectl delete pod; done
-
--- k8s-spec-scripts/run-etcd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --restart=always --name etcd -v /data/varlib/etcd -p 4001:4001 -d portworx/etcd:latest
